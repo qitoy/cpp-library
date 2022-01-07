ae3921caa413b2aa9d139ab9cc2253128094c0db
qitoy <su.kan@icloud.com>
2022-01-07 22:26:49 +0900

    fix

--- template/RollingHash.hpp
+++ template/RollingHash.hpp
@@ -13,10 +13,10 @@ class RollingHash {
 		: _n(S.size()), _base1(_n+1), _base2(_n+1), _hash1(_n+1), _hash2(_n+1) {
 			_base1[0]=_base2[0]=1;
 			for(int i=0; i<_n; i++) {
-				_base1[i+1]=mul(_base1[i],b1);
-				_base2[i+1]=mul(_base2[i],b2);
-				_hash1[i+1]=mul(_hash1[i],b1)+S[i];
-				_hash2[i+1]=mul(_hash2[i],b2)+S[i];
+				_base1[i+1]=mul(_base1[i],_b1);
+				_base2[i+1]=mul(_base2[i],_b2);
+				_hash1[i+1]=mul(_hash1[i],_b1)+S[i];
+				_hash2[i+1]=mul(_hash2[i],_b2)+S[i];
 			}
 		}
 	std::pair<u64,u64> gethash(int l, int r) {
@@ -30,7 +30,7 @@ class RollingHash {
 	private:
 	const u64 mod=(1ull<<61)-1;
 	int _n;
-	static u64 b1, b2;
+	static u64 _b1, _b2;
 	std::vector<u64> _base1, _base2, _hash1, _hash2;
 	u64 mul(u128 a, u128 b) const {
 		u128 t=a*b; t=(t>>61)+(t&mod);
@@ -38,5 +38,5 @@ class RollingHash {
 	}
 };
 
-unsigned long long RollingHash::b1=std::random_device()()+128,
-                   RollingHash::b2=std::random_device()()+128;
+unsigned long long RollingHash::_b1=std::random_device()()+128,
+                   RollingHash::_b2=std::random_device()()+128;
