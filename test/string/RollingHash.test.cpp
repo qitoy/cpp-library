af5734854f154a1e50bd4bb90c750482d4abb6b0
qitoy <su.kan@icloud.com>
2022-02-10 03:36:48 +0900

    add test(rh)

---
+++ test/string/RollingHash.test.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include"../../string/RollingHash.hpp"
+using namespace std;
+
+int main(){
+
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	string T,P; cin >> T >> P;
+	RollingHash Th(T), Ph(P);
+	int N=T.length(), M=P.length();
+	for (int i = 0; i+M <= N; i++) 
+		if(Th.gethash(i,i+M)==Ph.gethash(0,M))
+			cout << i << '\n';
+
+
+}
