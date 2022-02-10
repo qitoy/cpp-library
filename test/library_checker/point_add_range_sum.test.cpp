7529b48c0ba1c95a77bcf7e5fd0f9267e32c2458
qitoy <su.kan@icloud.com>
2022-02-10 22:32:59 +0900

    add

---
+++ test/library_checker/point_add_range_sum.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+#include<bits/stdc++.h>
+#include"../../data_structure/fenwicktree.hpp"
+using namespace std;
+
+int main(){
+
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int N,Q; cin >> N >> Q;
+	fenwick_tree<long long> F(N);
+	for (int i = 0; i < N; i++) {
+		long long a; cin >> a;
+		F.modify(i, a);
+	}
+	while(Q--) {
+		int q; cin >> q;
+		if(q==0) {
+			int p; long long x; cin >> p >> x;
+			F.add(p, x);
+		}
+		if(q==1) {
+			int l, r; cin >> l >> r;
+			cout << F.sum(l, r) << '\n';
+		}
+	}
+
+}
