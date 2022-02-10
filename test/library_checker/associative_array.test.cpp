c4ca5f0b6d356593b766e10aceff192e0da76f4c
qitoy <su.kan@icloud.com>
2022-02-10 22:53:58 +0900

    add

---
+++ test/library_checker/associative_array.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/associative_array"
+#include<bits/stdc++.h>
+#include"../../vector/coordinate_compression.hpp"
+using namespace std;
+
+int main(){
+
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int Q; cin >> Q;
+	vector<pair<int, long long>> Query(Q);
+	vector<long long> K(Q);
+	for (int i = 0; i < Q; i++) {
+		auto&& [q,v]=Query[i];
+		cin >> q >> K[i];
+		if(q==0) cin >> v;
+	}
+	coordinate_compression(K);
+	vector<long long> ans(K.size());
+	for (int i = 0; i < Q; i++) {
+		auto&& [q,v]=Query[i];
+		if(q==0) ans[K[i]]=v;
+		if(q==1) cout << ans[K[i]] << '\n';
+	}
+
+}
