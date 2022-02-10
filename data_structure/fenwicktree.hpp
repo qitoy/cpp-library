693180e55361fb0ff6a2cb2ee6b8285fd25db682
qitoy <su.kan@icloud.com>
2022-02-11 03:46:25 +0900

    add

--- data_structure/fenwicktree.hpp
+++ data_structure/fenwicktree.hpp
@@ -35,7 +35,17 @@ template <class T> struct fenwick_tree {
         return sum(r) - sum(l);
     }
 
-	// int lower_bound(T x) { }
+	int lower_bound(T x) {
+		if(x <= 0) return -1;
+		int l=0;
+		for(int k=1<<(32-__builtin_clz(_n)); k>0; k>>=1) {
+			if(l+k <= _n and data[l+k-1] < x) {
+				x-=data[l+k-1];
+				l+=k;
+			}
+		}
+		return l;
+	}
 
   private:
     int _n;
---
+++ test/library_checker/predecessor_problem.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/predecessor_problem"
+#include<bits/stdc++.h>
+#include"../../data_structure/fenwicktree.hpp"
+using namespace std;
+
+int main(){
+
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	int N,Q; cin >> N >> Q;
+	string T; cin >> T;
+	fenwick_tree<int> F(N);
+	for (int i = 0; i < N; i++) 
+		F.add(i, T[i]-'0');
+	while(Q--) {
+		int c,k; cin >> c >> k;
+		if(c==0) F.modify(k, 1);
+		if(c==1) F.modify(k, 0);
+		if(c==2) cout << F.get(k);
+		if(c==3) {
+			int l=F.lower_bound(F.sum(k)+1);
+			cout << (l==N?-1:l);
+		}
+		if(c==4) cout << F.lower_bound(F.sum(k+1));
+		cout << '\n';
+	}
+
+}
