6b1b330d9b68cc7eff18e9728c2d23d0cb5d237d
qitoy <su.kan@icloud.com>
2022-02-10 13:57:24 +0900

    add

---
+++ doc/tree/fenwicktree.md
@@ -0,0 +1,37 @@
+---
+title: Fenwick Tree
+documentation_of: //tree/fenwicktree.hpp
+---
+
+# 注釈
+
+これはACLのを改造したやつ
+
+# 機能
+
+型`T`を`unsigned`に変換する機能を削除  
+各値は非負でなくてはならない
+
+## `add(p,x)`
+
+そのまんま
+
+## `modify(p,x)`
+
+位置`p`に`x`を代入する
+
+## `get(i)`
+
+位置`i`の値を取得
+
+## `sum(r)`
+
+`[0,r)`の総和
+
+## `sum(l,r)`
+
+`[l,r)`の総和
+
+## `lower_bound(x)`
+
+`x`以上の位置を探す
---
+++ tree/fenwicktree.hpp
@@ -0,0 +1,54 @@
+// fenwicktree(ACL) modified
+template <class T> struct fenwick_tree {
+  public:
+    fenwick_tree() : _n(0) {}
+    explicit fenwick_tree(int n) : _n(n), data(n) {}
+
+    void add(int p, T x) {
+        assert(0 <= p && p < _n);
+        p++;
+        while (p <= _n) {
+            data[p - 1] += x;
+			assert(0 <= data[p-1]);
+            p += p & -p;
+        }
+    }
+
+	void modify(int p, T x) {
+		add(p, x-get(p));
+	}
+
+	T get(int i) {
+		return sum(i, i+1);
+	}
+
+    T sum(int r) {
+        T s = 0;
+        while (r > 0) {
+            s += data[r - 1];
+            r -= r & -r;
+        }
+        return s;
+    }
+
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        return sum(r) - sum(l);
+    }
+
+	int lower_bound(T x) {
+		if(x <= 0) return 0;
+		int i=0;
+		for(int k=1<<(32-__builtin_clz(_n)); k>0; k>>=1) {
+			if(i+k <= _n && data[i+k-1] < x) {
+				x -= data[i+k-1];
+				i += k;
+			}
+		}
+		return i;
+	}
+
+  private:
+    int _n;
+    std::vector<T> data;
+};
